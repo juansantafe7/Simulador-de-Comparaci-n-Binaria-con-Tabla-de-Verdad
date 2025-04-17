c7d8d3bdbb34f08703466e7686c8d114eb40e9c1
fabri404 <124693109+fabri404@users.noreply.github.com>
2025-04-16 22:14:15 -0300

    Conversion a binario
    Se Agrega la conversion de decimal a binario y se modifica la condicion del la validacion, para que tambien tome numneros negativos

--- Simulador_De_Comparacion_Binaria.py
+++ Simulador_De_Comparacion_Binaria.py
@@ -1,10 +1,10 @@
 print("Conversor de numeros base 10 a otro sistemas numérico")
 # 1. Ingreso de datos
-numero_a_convertir = input("Ingrese un numero entero positivo:\n")
+numero_a_convertir = (input("Ingrese un numero entero:\n"))
 
 # 2. Validación
-while not numero_a_convertir.isdigit():
-  numero_a_convertir = input("Error: Por favor, ingresa un numero entero positivo:\n")
+while not (numero_a_convertir.isdigit() or (numero_a_convertir[0] == "-" and numero_a_convertir[1:].isdigit())):
+    numero_a_convertir = input("Error: Por favor, ingresa un número entero válido:\n")
 
 
 # 3. Selector de conversor
@@ -17,16 +17,38 @@ opcion_a_convertir = int(input(""))
 while opcion_a_convertir != 1 and opcion_a_convertir != 2 and opcion_a_convertir != 3:
    print("La opción ingresada es incorrecta")
    opcion_a_convertir = int(input("Ingrese 1, 2 o 3:\n"))
+   
+numero_a_convertir = int(numero_a_convertir) 
 
 # 4. Conversion a bianrio (Fabricio)
+if opcion_a_convertir == 1:
+    print("=== Conversión manual de entero a binario ===\n")
+    if numero_a_convertir == 0:
+        print("El binario de 0 es: 0")
+    elif numero_a_convertir < 0:
+        binario = ""
+        numero_a_convertir = -numero_a_convertir  # lo pasamos a positivo para operar
 
+        while numero_a_convertir > 0:
+            residuo = numero_a_convertir % 2
+            binario = str(residuo) + binario
+            numero_a_convertir = numero_a_convertir // 2
+
+        binario = "-" + binario  # Agregamos el signo negativo al resultado
+        print("El número binario equivalente es:", binario)
+    else:
+        binario = ""
+        while numero_a_convertir > 0:
+            residuo = numero_a_convertir % 2
+            binario = str(residuo) + binario
+            numero_a_convertir = numero_a_convertir // 2
+        print("El número binario equivalente es:", binario)
+        
 # 5. Conversion a hexadecimal (Gabi)
 
 # 6. Conversion a Octal (Juan)
 
 # 7. Mostrar resultado
-
-
 
 
 
