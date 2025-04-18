75366fa7e63289cecb8dc02b6b8832e075edb2f7
Gabriel <loveragabriel20@gmail.com>
2025-04-18 13:06:25 -0300

    conversor a decimal a Hexadecimal

--- Simulador_De_Comparacion_Binaria.py
+++ Simulador_De_Comparacion_Binaria.py
@@ -45,7 +45,47 @@ if opcion_a_convertir == 1:
         print("El número binario equivalente es:", binario)
         
 # 5. Conversion a hexadecimal (Gabi)
-
+if opcion_a_convertir == 3:
+   print(f"=== Conversión manual de entero {numero_a_convertir} a Hexadecimal  ===\n")
+   #Evaluamos si el número ingresado es menor a 10 y asignamos su valor Hexadecimal sin hacer conversión.
+   if numero_a_convertir < 10:
+      print(f"El número decimal {numero_a_convertir}, es igual en hexadecimal")
+   if numero_a_convertir == 15:
+      print(f"El número decimal {numero_a_convertir}, es la letra F en Haxadecimal")
+   if numero_a_convertir == 14:
+      print(f"El número decimal {numero_a_convertir}, es la letra E n Haxadecimal")
+   if numero_a_convertir == 13:
+      print(f"El número decimal {numero_a_convertir}, es la letra D en Hexadecimal")
+   if numero_a_convertir == 12:
+      print(f"El número decimal {numero_a_convertir}, es la letra C en Hexadecimal")
+   if numero_a_convertir == 11:
+      print(f"El número decimal {numero_a_convertir}, es la letra B en Hexadecimal")
+   if numero_a_convertir == 10:
+      print(f"El número decimal {numero_a_convertir}, es la letra A en Hexadecimal")
+   if numero_a_convertir < 10:
+      print(f" El número decimal {numero_a_convertir} es igual en el sistema Hexadecimal")
+   #En el caso que el número ingresado sea mayor a 10, dividimos entre 16 para hacer la conversión
+   else:
+        #Armamos una lista para ingresar las letras que corresponden a los número decimales desde el 10 al 15
+        letras =["A", "B", "C", "D", "E", "F"]
+        #Creamos una lista para ir guardando el resultado de la divsión del número decimal a hexadecimal. 
+        numero_hexadecimal = []
+        #Mientrás el número sea mayor a 15 evaluamos dividimos entre 16.
+        while numero_a_convertir > 0:
+              #Se almacena el residuo de la división
+            residuo = numero_a_convertir % 16
+            #Evaluamos si el residuo es menor a 10 y lo convertimos en string para almecenarlo en la lista de numero_hexadecimal.
+            if residuo < 10: 
+                 digito = str(residuo)
+            #Cuando el residuo sea entre 10 y 15, le restaremos 10 a la lista de letras y nos dará el indice de la letra que corresponda. 
+            else:
+                 digito = letras[residuo-10]
+            #Una vez evaluadas todas las condiciones insertamos el valor del dígito en la variable numero_hexadecimal como lista. 
+            numero_hexadecimal.insert(0, digito)
+            # Dividimos el número decimal entre 16, y e2025l resultado lo asigamos a la variable numero_a_convertir para evaluar nuevamente la condición While. 
+            numero_a_convertir = numero_a_convertir // 16
+        #Finalmente unimos todos los valores de residuos guardados en la varible mientras se ejecutó el bucle While
+        print("El número en hexadecimal es:", "".join(numero_hexadecimal))
 # 6. Conversion a Octal (Juan)
 
 elif opcion_a_convertir == 2:
