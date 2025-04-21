938017055a9241087d505b4bcf81be44ab16c1b9
GastonLell <gastonglell@gmail.com>
2025-04-21 20:34:54 -0300

    refactor

--- Simulador_De_Comparacion_Binaria.py
+++ Simulador_De_Comparacion_Binaria.py
@@ -1,11 +1,33 @@
+# helper
+def mostrar_mensaje(a_convertir, opcion, result):
+    base = ''
+    if opcion == 1:
+        base = 'binario'
+    if opcion == 2:
+        base = 'octal'
+    if opcion == 3:
+        base = 'hexadecimal'
+
+    print(f"El {a_convertir} en {base} es {result}")
+
+
 print("Conversor de numeros base 10 a otro sistemas numérico")
 # 1. Ingreso de datos
 numero_a_convertir = (input("Ingrese un numero entero:\n"))
 
-# 2. Validación
+# 2. Validaciones (validamos que sea un numero)
 while not (numero_a_convertir.isdigit() or (numero_a_convertir[0] == "-" and numero_a_convertir[1:].isdigit())):
     numero_a_convertir = input("Error: Por favor, ingresa un número entero válido:\n")
 
+# convertimos el stirng a numero, lo hacemos despues de la validacion de numero entero para que no crashee si es flotante
+numero_a_convertir = int(numero_a_convertir)
+
+#  verificamos si el numero es negativo
+es_negativo = numero_a_convertir < 0
+
+# guardamos el numero en una variable auxiliar para poder operarla y modificarla
+# de esta manera guardamos el original para futuras operaciones
+numero_a_convertir_aux = (-numero_a_convertir) if es_negativo else numero_a_convertir
 
 # 3. Selector de conversor
 print("Ingresa el numero de la opción a la que quieres convertir")
@@ -14,91 +36,59 @@ print("2) Base 8")
 print("3) Base 16")
 opcion_a_convertir = int(input(""))
 
+# verificamos que la opcion ingresada sea correcta
 while opcion_a_convertir != 1 and opcion_a_convertir != 2 and opcion_a_convertir != 3:
-   print("La opción ingresada es incorrecta")
-   opcion_a_convertir = int(input("Ingrese 1, 2 o 3:\n"))
-   
-numero_a_convertir = int(numero_a_convertir) 
+    print("La opción ingresada es incorrecta")
+    opcion_a_convertir = int(input("Ingrese 1, 2 o 3:\n"))
 
-# 4. Conversion a bianrio (Fabricio)
-if opcion_a_convertir == 1:
-    print("=== Conversión manual de entero a binario ===\n")
-    if numero_a_convertir == 0:
-        print("El binario de 0 es: 0")
-    elif numero_a_convertir < 0:
+# validar si es cero
+if numero_a_convertir == 0:
+    mostrar_mensaje(numero_a_convertir, opcion_a_convertir, numero_a_convertir)
+else:
+    # 4. Conversion a bianrio (Fabricio)
+    if opcion_a_convertir == 1:
+        print("=== Conversión manual de entero a binario ===\n")
+
         binario = ""
-        numero_a_convertir = -numero_a_convertir  # lo pasamos a positivo para operar
-
-        while numero_a_convertir > 0:
-            residuo = numero_a_convertir % 2
+        while numero_a_convertir_aux > 0:
+            residuo = numero_a_convertir_aux % 2
             binario = str(residuo) + binario
-            numero_a_convertir = numero_a_convertir // 2
+            numero_a_convertir_aux = numero_a_convertir_aux // 2
 
-        binario = "-" + binario  # Agregamos el signo negativo al resultado
-        print("El número binario equivalente es:", binario)
-    else:
-        binario = ""
-        while numero_a_convertir > 0:
-            residuo = numero_a_convertir % 2
-            binario = str(residuo) + binario
-            numero_a_convertir = numero_a_convertir // 2
-        print("El número binario equivalente es:", binario)
+        mostrar_mensaje(numero_a_convertir, opcion_a_convertir, f"-{binario}" if es_negativo else binario)
+
+    # 6. Conversion a Octal (Juan)
+    elif opcion_a_convertir == 2:
+        print("=== Conversión manual de entero a octal ===\n")
+        octal = ""
+
+        while numero_a_convertir_aux > 0:
+            residuo = numero_a_convertir_aux % 8
+            octal = str(residuo) + octal
+            numero_a_convertir_aux = numero_a_convertir_aux // 8
+
+        mostrar_mensaje(numero_a_convertir, opcion_a_convertir, f"-{octal}" if es_negativo else octal)
+
+    # 5. Conversion a hexadecimal (Gabi)
+    elif opcion_a_convertir == 3:
+        print(f"=== Conversión manual de entero {numero_a_convertir} a Hexadecimal  ===\n") 
         
-# 5. Conversion a hexadecimal (Gabi)
-if opcion_a_convertir == 3:
-   print(f"=== Conversión manual de entero {numero_a_convertir} a Hexadecimal  ===\n")
-   if numero_a_convertir == 0:
-      print(f"El número Hexadecimal equivalente es: 0")
-   else:
-        numero_negativo = False
-        #Se evalua si el número ingresado es negativo, en caso de ser cierto se pasa a positivo para hacer la conversión.
-        if numero_a_convertir < 0:
-            numero_negativo = True
-            numero_a_convertir = numero_a_convertir * -1
         #Se definen letras para valores especiales 10, 11, 12, 13, 14, 15
         letras =["A", "B", "C", "D", "E", "F"]
         numero_hexadecimal = ""
         #Ciclo While para evaluar cada número positivo
-        while numero_a_convertir > 0:
-            residuo = numero_a_convertir % 16
+        while numero_a_convertir_aux > 0:
+            residuo = numero_a_convertir_aux % 16
             if residuo < 10: 
-                 digito = str(residuo)
+                digito = str(residuo)
             else:
-                 digito = letras[residuo-10]
+                digito = letras[residuo-10]
+
             numero_hexadecimal = digito + numero_hexadecimal
-            numero_a_convertir = numero_a_convertir // 16
-        if numero_negativo:
-            numero_hexadecimal = "-" + numero_hexadecimal
-        print(f"El número en hexadecimal equivalente es: {numero_hexadecimal}")
-
-# 6. Conversion a Octal (Juan)
-
-elif opcion_a_convertir == 2:
-    print("=== Conversión manual de entero a octal ===\n")
-    if numero_a_convertir == 0:
-        print("El número octal de 0 es: 0")
-    elif numero_a_convertir < 0:
-        octal = ""
-        numero_a_convertir = -numero_a_convertir
-
-        while numero_a_convertir > 0:
-            residuo = numero_a_convertir % 8
-            octal = str(residuo) + octal
-            numero_a_convertir = numero_a_convertir // 8
-
-        octal = "-" + octal
-        print("El número octal equivalente es:", octal)
+            numero_a_convertir_aux = numero_a_convertir_aux // 16
+            
+        mostrar_mensaje(numero_a_convertir, opcion_a_convertir, f"-{numero_hexadecimal}" if es_negativo else numero_hexadecimal)
     else:
-        octal = ""
-        while numero_a_convertir > 0:
-            residuo = numero_a_convertir % 8
-            octal = str(residuo) + octal
-            numero_a_convertir = numero_a_convertir // 8
-
-        print("El número octal equivalente es:", octal)
-
-
-# 7. Mostrar resultado
-
+        print("La opción ingresada no es correcta")
 
 
